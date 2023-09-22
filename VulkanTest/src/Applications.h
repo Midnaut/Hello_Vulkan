3a7e223f81bdad6ac0da02e8fd8c189ffa7bbc16
Luke Davenport <luke.l.davenport@gmail.com>
2023-09-21 20:51:03 -0700

    Validation layers continued

--- VulkanTest/src/Applications.h
+++ VulkanTest/src/Applications.h
@@ -2,7 +2,7 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
+#include <vector>
 
 class HelloTriangleApplication {
 	public:
@@ -10,13 +10,34 @@ class HelloTriangleApplication {
 	private:
 		void initWindow();
 		void initVulkan();
+		std::vector<const char*> getRequiredExtensions();
 		void createInstance();
+		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+		void setupDebugMessenger();
+		bool checkValidationLayerSupport();
 		void mainLoop();
 		void cleanup();
+
+		static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+			VkDebugUtilsMessageTypeFlagsEXT messageType,
+			const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+			void* pUserData);
 
 		GLFWwindow* window;
 		const uint32_t WINDOW_WIDTH = 800;
 		const uint32_t WINDOW_HEIGHT = 600;
 
 		VkInstance instance;
+		VkDebugUtilsMessengerEXT debugMessenger;
+
+		const std::vector<const char*> validationLayers = {
+			"VK_LAYER_KHRONOS_validation"
+		};
+
+		#ifdef NDEBUG
+		const bool enableValidationLayers = false;
+		#else
+		const bool enableValidationLayers = true;
+		#endif
 };
